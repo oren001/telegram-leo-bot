8ed9fd980252feac0064870e1536ec2019d11d87
Oren Weiss <oren001@gmail.com>
2025-12-11 02:37:10 +1100

    Configure correct port and procfile for Render

--- keep_alive.py
+++ keep_alive.py
@@ -1,14 +1,8 @@
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "I am alive!"
+import os
 
 def run():
-    app.run(host='0.0.0.0', port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
